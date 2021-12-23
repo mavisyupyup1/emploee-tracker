ef285a691d29a52958936e1b67d2577b9ec785c5
Your Name <graceliu@gracelius-iMac.home>
2021-12-23 15:50:10 -0500

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -22,3 +22,9 @@ CREATE TABLE employee(
    FOREIGN KEY (manager_id) REFERENCES employee(id) 
     ON DELETE SET NULL ON UPDATE CASCADE
 );
+
+SELECT name FROM department LEFT JOIN role ON department.id = role.department_id;
+
+SELECT title, salary, department_id FROM role LEFT JOIN department ON department.id = role.department_id;
+
+SELECT first_name, last_name, role_id, manager_id FROM employee JOIN role ON employee.role_id = role.id;
